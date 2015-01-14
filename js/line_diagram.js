7effa7eb84b4d09d90b1d34afa8d0c67826b26b9
Vasily Nesterov <da3x11@gmail.com>
2015-01-14 12:23:23 +0300

    aaa

--- js/line_diagram.js
+++ js/line_diagram.js
@@ -5,56 +5,6 @@
 angular.module('diagram',[])
     .directive('lineDiagram', function($compile) {
 
-        function getContentData(data, scale, from) {
-
-            var content = {
-                    rects: [],
-                    labels: []
-                },
-                label_y = 33,
-                rect_y = 15,
-                delta_y = 30,
-                offset = 15.5,
-                labelsWidths = getLabelsWidths(data),
-                globalOffset = labelsWidths[0].val;
-
-            for(var i= 0, j= data.length; i<j; i++) {
-                var current = data[i],
-                    currentLabelOffset;
-
-                for(var i1= 0, j1= labelsWidths.length; i1<j1; i1++) {
-                    if(labelsWidths[i1].name == current.label) {
-                        currentLabelOffset = labelsWidths[i1].val;
-                    }
-                }
-
-                content.labels.push({
-                        text: current.label,
-                        y: label_y,
-                        x: globalOffset - currentLabelOffset
-                    });
-
-                for(var k= 0, l= current.data.length; k<l; k++) {
-                    var currentData = current.data[k],
-                        width = ((currentData.end - currentData.begin) / 1000) * scale,
-                        x = ((currentData.begin - from) / 1000) * scale + offset + globalOffset;
-
-                    content.rects.push({
-                        id: 'rect' + i,
-                        x: x,
-                        y: rect_y,
-                        width: width,
-                        color: currentData.color
-                    });
-                }
-
-                rect_y += delta_y;
-                label_y += delta_y;
-            }
-
-            return content;
-        }
-
         var getLabelsWidths = (function() {
             var result = [];
 
@@ -79,6 +29,59 @@ angular.module('diagram',[])
 
         })();
 
+        function getContentData(data, scale, from) {
+
+            var content = {
+                    rects: [],
+                    labels: []
+                },
+                label_y = 33,
+                rect_y = 15,
+                delta_y = 30,
+                offset = 15.5,
+                labelsWidths = getLabelsWidths(data),
+                globalOffset = labelsWidths[0].val,
+                rectIndex = 0;
+
+            for(var i= 0, j= data.length; i<j; i++) {
+                var current = data[i],
+                    currentLabelOffset;
+
+                for(var i1= 0, j1= labelsWidths.length; i1<j1; i1++) {
+                    if(labelsWidths[i1].name == current.label) {
+                        currentLabelOffset = labelsWidths[i1].val;
+                    }
+                }
+
+                content.labels.push({
+                        text: current.label,
+                        y: label_y,
+                        x: globalOffset - currentLabelOffset
+                    });
+
+                for(var k= 0, l= current.data.length; k<l; k++) {
+                    var currentData = current.data[k],
+                        width = ((currentData.end - currentData.begin) / 1000) * scale,
+                        x = ((currentData.begin - from) / 1000) * scale + offset + globalOffset;
+
+                    content.rects.push({
+                        id: 'rect' + rectIndex,
+                        x: x,
+                        y: rect_y,
+                        width: width,
+                        color: currentData.color
+                    });
+
+                    rectIndex++;
+                }
+
+                rect_y += delta_y;
+                label_y += delta_y;
+            }
+
+            return content;
+        }
+
         function getStepOfTimeNet(from, to){
             var delta = (to - from) / (1000 * 60);
             // todo функция плохо работает с промежутками 1.50 и тд...
@@ -95,6 +98,23 @@ angular.module('diagram',[])
             }
         }
 
+        function getTimeNet(from, to, width, offset) {
+            var timeStep = getStepOfTimeNet(from, to),
+                stepsCount = ((to - from) / 1000 / 60) / timeStep,
+                landStep = (width - 36) / stepsCount,
+                result = [];
+
+            for (var step = 0, i = 0, t = from.valueOf(); step <= stepsCount; step++, i += (landStep), t += (timeStep * 60000)) {
+                var date = new Date(t);
+                result.push({
+                    // todo придумать как добавлять месяцы, годы и дни
+                    x: i + offset,
+                    label: ((date.getHours() < 10) ? '0' + date.getHours().toString() :  date.getHours().toString()) + ':' + ((date.getMinutes() < 10) ? '0' + date.getMinutes().toString() : date.getMinutes().toString())
+                });
+            }
+            return result;
+        }
+
         return {
             restrict: 'E',
             scope: {
@@ -109,7 +129,7 @@ angular.module('diagram',[])
 
                 var addResizeEvent = false;
 
-                if(scope.width.search('%')) {
+                if(scope.width.search('%') > -1) {
                     scope.width = element.parent().width() * (parseInt(scope.width, 10) / 100);
                     addResizeEvent = true;
                 }
@@ -121,39 +141,30 @@ angular.module('diagram',[])
 
                 if(addResizeEvent) {
                     angular.element(window).on('resize', function() {
-                        scope.resizer();
+                        if(scope.sizesChanged()) {
+                            scope.resizer();
+                        }
                     });
                     angular.element(window).on('click', function() {
-                        scope.resizer();
+                        if(scope.sizesChanged()) {
+                            scope.resizer();
+                        }
                     });
                 }
 
                 var windowOffset = getLabelsWidths(scope.data)[0].val;
 
                 var diagramWindowWidth = scope.width - windowOffset;
-
-                var timeStep = getStepOfTimeNet(scope.from, scope.to),
-                    stepsCount = ((scope.to - scope.from) / 1000 / 60) / timeStep,
-                    landStep = (diagramWindowWidth - 36) / stepsCount;
-
-                scope.positions = [];
-                for (var step = 0, i = 0, t = scope.from.valueOf(); step <= stepsCount; step++, i += (landStep), t += (timeStep * 60000)) {
-                    var date = new Date(t);
-                    scope.positions.push({
-                        // todo придумать как добавлять месяцы, годы и дни
-                        x: i + windowOffset,
-                        label: ((date.getHours() < 10) ? '0' + date.getHours().toString() :  date.getHours().toString()) + ':' + ((date.getMinutes() < 10) ? '0' + date.getMinutes().toString() : date.getMinutes().toString())
-                    });
-                }
-
+                scope.positions = getTimeNet(scope.from, scope.to, diagramWindowWidth, windowOffset);
                 scope.contentData = getContentData(
                         scope.data,
                         parseInt(diagramWindowWidth - 36) / ((scope.to - scope.from) / 1000),
                         scope.from
                     );
             },
-            controller: function($scope, $element) {
-                $scope.resizer = function(oldSize, newSize) {
+            controller: function($scope, $element, $attrs) {
+
+                $scope.resizer = function() {
                     var children = $element.children().map(function(index, elem) {
                         return $(elem);
                     }),
@@ -164,26 +175,47 @@ angular.module('diagram',[])
                         return element.is('rect');
                     });
 
+                    // todo сделать на это функуцию...
+                    var offseet = getLabelsWidths($scope.data)[0].val;
+                    $scope.width = $element.parent().width() * (parseInt($attrs.width, 10) / 100);
+                        newTimeNetPositions = getTimeNet($scope.from, $scope.to, $scope.width - offseet, offseet);
+
+                    // todo это тоже можно в функцию
                     timeNet.each(function() {
-                        // todo сюда пихать изменение положиеня временной сетки
+                        var exit = false,
+                            i = 0;
+                        while(!exit) {
+                            if($(this).text() == newTimeNetPositions[i].label && !exit) {
+                                $(this).attr('x', newTimeNetPositions[i].x);
+                                exit = true;
+                            }
+                            i++;
+                        }
                     });
 
-                    var newDiagramWindowWidth = newSize - getLabelsWidths(scope.data)[0].val,
-                        newScale = parseInt((newDiagramWindowWidth - 36)) / (($scope.to - $scope.from) / 1000);
-
-                    var newContent = getContentData($scope.data, newScale, $scope.from);
+                    var newDiagramWindowWidth = $scope.width - offseet,
+                        newScale = parseInt((newDiagramWindowWidth - 36)) / (($scope.to - $scope.from) / 1000),
+                        newContent = getContentData($scope.data, newScale, $scope.from).rects;
 
                     rects.each(function() {
                         var exit = false,
                             i = 0;
                         while(!exit) {
-                            if($(this).attr('id') == newContent[i].id) {
+                            if($(this).attr('id') == newContent[i].id && !exit) {
                                 $(this).attr('x', newContent[i].x);
                                 $(this).attr('width', newContent[i].width);
                                 exit = true;
                             }
+                            i++;
                         }
                     });
+                }
+
+                $scope.sizesChanged = function() {
+                    if($scope.width != $element.parent().width()) {
+                        return true;
+                    }
+                    return true;
                 }
             },
             template:
